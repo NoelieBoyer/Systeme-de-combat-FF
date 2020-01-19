37c5f4ae48f0797e7991cbed1d94512df1216a59
NoelieBoyer <59953453+NoelieBoyer@users.noreply.github.com>
2020-01-19 16:49:56 +0100

    Exercice-2
    Décrémentation PV du monstre

--- ff.c
+++ ff.c
@@ -4,18 +4,18 @@ int main(){
   int PointDeVieHero = 100;
   int PointDeVieMob = 25;
   printf("Vous avez %d PV\n", PointDeVieHero);
-  printf("Un monstre vous attaque\n");
+  printf("Un monstre vous bloque la route !\n");
   printf("Monstre : %d PV\n", PointDeVieMob);
   printf("Vous attaquez le monstre\n");
-  printf("-5 PV\n");
-<<<<<<< Updated upstream
-  PointDeVieMob=PointDeVieMob-5; 
-  printf("Monstre : %d PV\n",PointDeVieMob);
-=======
   PointDeVieMob=PointDeVieMob-5;
   printf("Monstre : %d PV\n",PointDeVieMob);
-  
->>>>>>> Stashed changes
+
+  while (PointDeVieMob>0) {
+    PointDeVieMob=PointDeVieMob-5;
+    printf("Monstre : %d PV\n", PointDeVieMob);
+  }
+  printf("Le monstre est mort ! Vous avez gagne ! \n");
+  printf("Vous avez %d PV\n", PointDeVieHero);
 
 
   return 0;
